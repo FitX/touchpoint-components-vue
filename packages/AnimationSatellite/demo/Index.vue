04c53f32e68bd7bc78af71f62a044a023f8f0166
Michael Gerstmann <michael.gerstmann@fitx.de>
2020-02-20 10:10:52 +0100

    first package

---
+++ packages/AnimationSatellite/demo/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="detail">
+    <h1>AnimationSatellite</h1>
+    <p>User Feedback in Form einer Animation. Momentan genutzt für Umfrage-Buttons.</p>
+    <button @click="animate = !animate">
+      Toggle Animation <animation-satellite :animation="animate"></animation-satellite>
+    </button>
+  </div>
+</template>
+
+<script>
+import AnimationSatellite from '../src/AnimationSatellite.vue';
+
+export default {
+  components: {
+    AnimationSatellite,
+  },
+  data() {
+    return {
+      animate: false,
+    };
+  },
+};
+</script>
+
+<style>
+  button {
+    position: relative;
+  }
+</style>
